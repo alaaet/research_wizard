9a87732c29f2ebc576618f7a5e18f666999a80ad
Alaa Abuiteiwi <a.abuiteiwi@occentus.net>
2025-05-05 11:15:24 +0200

    changed docasauros.conig.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://alaaet.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/research_wizard',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
